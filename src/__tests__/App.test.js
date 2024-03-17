ffdd153c14d5e07986c25ce09b251819b8da2a8d
JohnWayneMarshall <johnwayne100301@gmail.com>
2024-03-17 19:05:47 -0400

    Wrong quote used

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("John Marshall");
+    expect(document.title).toBe('John Marshall');
   });
 
   it('can navigate to /about', async () => {
