9e8821592fb0921745f80a1ca72431b4d48362eb
JohnWayneMarshall <73924240+JohnWayneMarshall@users.noreply.github.com>
2024-05-11 22:48:51 -0400

    Add new work experience

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -13,6 +13,20 @@
  */
 const work = [
   {
+    name: 'IYKYKnow.ai',
+    position: 'Technical Support / Junior Developer',
+    url: 'iykyknow.ai',
+    startDate: '2024-05',
+    summary: 'As a Technical Support / Junior Developer at IYKYKnow.ai, I directly contribute to the growth and effectiveness of AI-driven operations in a dynamic, startup environment. My role is crucial in addressing complex technical issues and enhancing our software solutions.',
+    highlights: [
+      'Analyze business requirements and customize software solutions to meet client specifications.',
+      'Participate in the management, troubleshooting, and support of AI software products.',
+      'Collaborate with development teams to ensure system compatibility and integrate new technologies.',
+      'Engage in continuous learning to stay ahead of emerging technologies, focusing on Python and JavaScript.',
+      'Provide key technical support to clients, solving a variety of software issues within cloud environments.',
+    ],
+  },
+  {
     name: 'Lowe’s',
     position: 'Sales Associate',
     url: 'https://www.lowes.com/',
